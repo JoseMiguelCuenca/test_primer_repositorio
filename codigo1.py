7d12584c23aa280bba77aedcf1b69f657923d6f1
Jose Miguel Cuenca fernandez <=josemiguelcuencafernandez33@gmail.com>
2025-06-18 18:53:21 +0200

    Subida a Git

---
+++ codigo1.py
@@ -0,0 +1,13 @@
+nombre = 'Jose'
+mascota = 'Tiger'
+edad = 34
+
+
+#Rama Develop
+def (nombre, edad):
+    print(f"Mi nombre es {nombre} y tengo {edad} años")
+
+#Rama experimental
+edadmascota = 5
+def(presentacion):
+    print(f"El nombre de mi mascota es {mascota} y tiene {edad} años")
