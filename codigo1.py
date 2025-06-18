53f66d8449f2f6e409d8ad603f0c74262d4f47c4
JoseMiguelCuenca <josemiguelcuencafernandez33@gmail.com>
2025-06-18 19:12:01 +0200

    Update codigo1.py

--- codigo1.py
+++ codigo1.py
@@ -1,13 +1,2 @@
-nombre = 'Jose'
-mascota = 'Tiger'
-edad = 34
-
-
-#Rama Develop
-def (nombre, edad):
-    print(f"Mi nombre es {nombre} y tengo {edad} años")
-
-#Rama experimental
-edadmascota = 5
-def(presentacion):
-    print(f"El nombre de mi mascota es {mascota} y tiene {edad} años")
+def decir_hola():
+    return "Hola!"
