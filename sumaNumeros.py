848ac34a29582ef470dc4a4aa9582a250207f288
Jose Miguel Cuenca fernandez <=josemiguelcuencafernandez33@gmail.com>
2025-06-19 19:13:33 +0200

    Subo el fichero sumaNumeros.py clase 3-4

---
+++ sumaNumeros.py
@@ -0,0 +1,7 @@
+# Ejercicio a completar para la clase 4
+'''Que el programa te pida dos números  y te devuelva la suma de los dos'''
+#Propuesta
+numero1 = int(input("Dime el primer número que quieres sumar:"))
+numero2 = int(input("Dime el segundo número:"))
+
+print(f"El número es:{numero1+numero2}")
