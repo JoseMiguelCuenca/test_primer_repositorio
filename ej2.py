73f43f7c31d54c1630345a779d5a80f987729bbf
Jose Miguel Cuenca fernandez <=josemiguelcuencafernandez33@gmail.com>
2025-06-24 17:45:45 +0200

    Añado las modificaciones con el ejercicio finalizado

--- ej2.py
+++ ej2.py
@@ -2,8 +2,16 @@
 quiero que me mostreis las notas, la primera, las tres ultimas, y las que van desde la segunda nota hasta la penultima'''
 
 notas = []
-for puntuacion in notas:
+while True:
     puntuacion = input("Introduce la nota a introducir (-1 si no hay más notas a introducir): ")
     if puntuacion != "-1":
         notas.append(int(puntuacion))
-    
+    else:
+        break
+
+# Muestra de la primera nota
+print(notas[0])
+# Muestra de las tres últimas:
+print(notas[-3:])
+# Muestra desde la segunda hasta la penúltima:
+print(notas[1:-1])
