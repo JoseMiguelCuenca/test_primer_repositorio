5754949c247ccec86b4e6c2a43ff360c21aa87da
Jose Miguel Cuenca fernandez <=josemiguelcuencafernandez33@gmail.com>
2025-06-26 13:16:53 +0200

    Subo contenido de EjerciciosClase7.py para actualizar fichero con los 4 ejercicios de la clase del 25 06 2025

--- Ejercicios clase7.py
+++ Ejercicios clase7.py
@@ -21,8 +21,7 @@ gato = Mascota("Gato","Europeo","7 vidas") # Creo la instancia Gato, generando u
 print(gato.animal)
 print(gato.raza)
 print(gato.vidas)'''
-
-# *****EJERCICIO 2*****
+'''# *****EJERCICIO 2*****
 # =====Crear dos personajes y un combate entre ambos===========================
 # (pasos para llevarlo a cabo):
 #======1. Crear una clase Personaje OK=========================================
@@ -53,8 +52,8 @@ class Personaje:
             luchador_2 = input("Introduce el nombre del segundo luchador: ")
             if luchador_2 in combatientes:
                 return "Los dos luchadores están preparados, que comience el combate!"
-                '''En este punto añadiré más adelante que no se puede seleccionar el combatiente seleccionado como luchador_1 +
-                dado que un personaje no debería poder luchar contra sí mismo!'''
+                # En este punto añadiré más adelante que no se puede seleccionar el combatiente seleccionado como luchador_1 +
+                # dado que un personaje no debería poder luchar contra sí mismo!
             else:
                 return "El segundo personaje no ha sido creado, créalo como luchador antes de iniciar un combate con él"
 # Creación de dos instancias (dos posibles combatientes)
@@ -63,15 +62,14 @@ Hulk = Personaje ("Hulk", 15,5,85)
 # Checkpoint (OK)
 print(combatientes)
 # Checkpoint2 (OK)
-'''No entiendo por que le tengo que llamar desde Thor, si quiero que sea una opcion de interacción para iniciarla
-Cuando quiera, sin necesidad de llamar a una instancia'''
+# No entiendo por que le tengo que llamar desde Thor, si quiero que sea una opcion de interacción para iniciarla
+# Cuando quiera, sin necesidad de llamar a una instancia
 Combate = Thor.seleccion_personajes_combate()
 print(Combate)
-    # Pendiente separar el método de selección del de validación 
-
-'''
+    # Pendiente separar el método de selección del de validación
+    
     def validar_personajes(self): # Comprobar que se han creado y su nombre está en combatientes
-       ...
+    """   ...
        idea:
        # inicioestructuraidea
        def seleccion_personajes_combate():
@@ -86,40 +84,50 @@ print(Combate)
             print("Uno o los dos personajes introducidos no han sido creados, créalos primero")
             return
         # finestructuraidea
-        '''
-
+        """
     def combate(self, atacante: Personaje, defensor: Personaje): 
             #Dinámica del combate, he indicar las clases ya que utilizaré los atributos que la contienen
         ...
+    
+    def combate(atacante: Personaje, defensor: Personaje):
+        turno = 0
+        while atacante.vida > 0 and defensor.vida > 0:
+            if turno % 2 == 0: # turno del atacante
+                daño = (atacante.ataque - defensor.defensa)
+                defensor.vida =- daño
+                print(f"{atacante.nombre} ataca a {defensor.nombre} y reduce su vida a {defensor.vida}")
+            else: #turno del defensor
+                daño = defensor.ataque - atacante.defensa
+                atacante.vida=-daño
+                print(f"{defensor.nombre} ataca a {atacante.nombre} y reduce su vida a {atacante.vida}")
+            turno=+1
+        if atacante.vida <= 0 and defensor.vida <=0:
+            print("Empate! Ambos combatientes han caído en el mismo turno!")
+        elif atacante.vida <= 0:
+            print(f"{defensor.nombre} ha ganado el combate!")
+        else:
+            print(f"{atacante.nombre} ha ganado el combate!")
+    
+seleccion_personajes_combate()
 '''
-'''def combate(atacante: Personaje, defensor: Personaje):
-    turno = 0
-    while atacante.vida > 0 and defensor.vida > 0:
-        if turno % 2 == 0: # turno del atacante
-            daño = (atacante.ataque - defensor.defensa)
-            defensor.vida =- daño
-            print(f"{atacante.nombre} ataca a {defensor.nombre} y reduce su vida a {defensor.vida}")
-        else: #turno del defensor
-            daño = defensor.ataque - atacante.defensa
-            atacante.vida=-daño
-            print(f"{defensor.nombre} ataca a {atacante.nombre} y reduce su vida a {atacante.vida}")
-        turno=+1
-    if atacante.vida <= 0 and defensor.vida <=0:
-        print("Empate! Ambos combatientes han caído en el mismo turno!")
-    elif atacante.vida <= 0:
-        print(f"{defensor.nombre} ha ganado el combate!")
-    else:
-        print(f"{atacante.nombre} ha ganado el combate!")
+'''# *****EJERCICIO 3*****
+#====Crear un coche======================================================
+#====Crear una persona===================================================
+#====La persona va a conducir el coche#==================================
+#====Habra luego mas personas que seran los pasajeros#===================
+#====TRUCO: Las ruedas, volante, etc... pueden ser objetos tambien!======
 
-seleccion_personajes_combate()'''
+pendiente hacer
+''''''
 
-'''#Ultimoejercicio
-#Hay que hacer una clase de Tajamar que tenga alumnos, uno debe ser dataclass(alumnos) y el otro no (cursos)
-
-
+'''#*****EJERCICIO 4*****
+'''# *****EJERCICIO 4*****
+#========Hay que hacer una clase de Tajamar que tenga alumnos===========
+#========uno debe ser dataclass(alumnos) y el otro no (cursos)==========
 
 import dataclasses
-# Dataclass
+
+@dataclasses.dataclass # Decorador: Decora una funcionalidad aumenta la forma de trabajar, de forma visual, etc.
 
 class Alumno(): # Dataclase
     nombre: str
@@ -127,22 +135,8 @@ class Alumno(): # Dataclase
     apellido2: str
     edad: int
     curso: str
-    def Curso_Tajamar(Alumno, nombre_curso: str, profesor: str, horas: int):
+
+    def Curso_Tajamar(self, Alumno, nombre_curso: str, profesor: str, horas: int): # Se incluye el sel(para cualquier método de clase)
         nombre_curso.curso = "Python"
 
-Alumno1 = Alumno(nombre="José Miguel", apellido1 ="Cuenca",apellido2 ="Fernández", edad=34, curso="Python")
-
-import dataclasses
-
-# Dataclass
-@dataclasses.dataclass
-
-class Persona():  # dataclass!
-    edad: int
-    nombre: str = ""
-
-    def comer(self):
-        ...
-
-heily = Persona(nombre="Heily", edad=22)
-print(heily.nombre)'''
+Alumno1 = Alumno(nombre="José Miguel", apellido1 ="Cuenca",apellido2 ="Fernández", edad=34, curso="Python")'''
